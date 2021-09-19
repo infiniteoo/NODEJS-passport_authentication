e8d5cee55c16366168e2af908f2980b842d3c581
Troy Dorman <troydorman@gmail.com>
2021-09-19 11:01:32 -0600

    booted up basic express server

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 // initialize dotenv
 require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
