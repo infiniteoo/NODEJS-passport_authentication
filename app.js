fd57bf557d4053384a7914c23f6b74455d974385
Troy Dorman <troydorman@gmail.com>
2021-09-19 11:14:11 -0600

    integrated ejs view engine.  created views folder with ejs files in them

--- app.js
+++ app.js
@@ -2,9 +2,16 @@
 require("dotenv").config();
 
 const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
 const app = express();
+
+// EJS
+app.use(expressLayouts);
+app.set("view engine", "ejs");
+
 const PORT = process.env.PORT || 5000;
 
+// ROUTES
 app.use("/", require("./routes/index"));
 app.use("/users", require("./routes/users"));
 
